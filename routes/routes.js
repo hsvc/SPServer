395986450e34f09d7448636f7597fd4445cc3cce
sand116 <sandsand1166@gmail.com>
2019-08-15 01:18:46 -0700

    python-shell code add

--- routes/routes.js
+++ routes/routes.js
@@ -10,6 +10,15 @@ delete
 */
 
 var fs = require('fs');
+var PythonShell = require('python-shell');
+options = {
+        mode: 'text',
+        // pythonPath: 'path/to/python' - 개발 서버에 맞게 설정
+        pythonOptions: ['-u'], // get print results in real-time
+        scriptPath: './CycleGAN-tensorflow',
+        args: ['--dataset_dir=monet2photo', '--phase=test', '--which_direction=BtoA']
+      };
+       
 
 module.exports = function(app) {
         app.get('/',function(req,res){
@@ -31,7 +40,16 @@ module.exports = function(app) {
                                                 }
                                         });
                         });
-        });
+                        
+
+
+                        PythonShell.run('main.py', options, function (err) {
+                                if (err) throw err;
+                                // results is an array consisting of messages collected during execution
+                                else  console.log('test success');
+                              });
+        
+                });
 
 
         app.get('/uploads/:file', function (req, res){
