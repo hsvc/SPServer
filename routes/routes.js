db856b078fbf2618d964d2eb0bb9134c5f576dea
jihyyeshin <tlsjh082@gmail.com>
2019-08-20 17:11:07 +0900

    M, C, V processing implemented

--- routes/routes.js
+++ routes/routes.js
@@ -54,10 +54,9 @@ module.exports = function(app) {
                             }
                     });
                 });
-	
+                // Monet 
                 var options = {
                     mode: 'text',
-                    // pythonPath: "C:\\Python34\\python.exe", //window path
                     pythonPath: "/usr/bin/python3",    //ubuntu path
                     pythonOptions: ['-u'],
                     scriptPath: './CycleGAN-tensorflow-master',    // 실행할 py 파일 path. 현재 nodejs파일과 같은 경로에 있어 생략
@@ -68,6 +67,33 @@ module.exports = function(app) {
                     // results is an array consisting of messages collected during execution
                     console.log('results: %j', results);
                 });
+                // Van gogh
+                options = {
+                    mode: 'text',
+                    pythonPath: "/usr/bin/python3",    //ubuntu path
+                    pythonOptions: ['-u'],
+                    scriptPath: './CycleGAN-tensorflow-master',    // 실행할 py 파일 path. 현재 nodejs파일과 같은 경로에 있어 생략
+                    args: ['--dataset_dir=vangogh2photo','--phase=test', '--which_direction=BtoA']
+                };
+                ps.PythonShell.run('main.py', options, function (err, results) {
+                    if (err) throw err;
+                    // results is an array consisting of messages collected during execution
+                    console.log('results: %j', results);
+                });
+                // Cezanne
+                options = {
+                    mode: 'text',
+                    pythonPath: "/usr/bin/python3",    //ubuntu path
+                    pythonOptions: ['-u'],
+                    scriptPath: './CycleGAN-tensorflow-master',    // 실행할 py 파일 path. 현재 nodejs파일과 같은 경로에 있어 생략
+                    args: ['--dataset_dir=cezanne2photo','--phase=test', '--which_direction=BtoA']
+                };
+                ps.PythonShell.run('main.py', options, function (err, results) {
+                    if (err) throw err;
+                    // results is an array consisting of messages collected during execution
+                    console.log('results: %j', results);
+                });
+
         });
 
         app.get('/uploads/:file', function (req, res){
